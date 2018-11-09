42fdadab37428ea08d493079d0519dd3a635536f
Kieran Healy <kieran.healy2@mycit.ie>
2018-11-09 12:30:26 +0000

    first

---
+++ src/main/java/ie/kieran/Config.java
@@ -0,0 +1,30 @@
+package ie.kieran;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class Config implements WebMvcConfigurer  {
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci= new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public CookieLocaleResolver localeResolver() {
+		CookieLocaleResolver localeResolver= new CookieLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.UK);
+		return localeResolver;
+	}}
